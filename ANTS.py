89ed2e2f3ed5be1a913ffab1094c99356b18466c
tohdon <74751540+tohdon@users.noreply.github.com>
2022-04-30 21:03:06 +0800

    Add files via upload

---
+++ ANTS.py
@@ -0,0 +1,115 @@
+import copy, random, sys, time
+try:
+    import bext
+except ImportError:
+    print("missing bext module")
+    sys.exit()
+    
+WIDTH, HEIGHT = bext.size()
+WIDTH -= 1
+HEIGHT -= 1
+NUMBER_OF_ANTS = 1
+PAUSE_AMOUNT = 0.1
+ANT_UP = '^'
+ANT_DOWN = 'v'
+ANT_LEFT = '<'
+ANT_RIGHT = '>'
+ANT_COLOR = 'red'
+BLACK_TILE = 'black'
+WHITE_TILE = 'white'
+
+NORTH = 'north'
+SOUTH = 'south'
+EAST = 'east'
+WEST = 'west'
+
+def main():
+    bext.fg(ANT_COLOR)
+    bext.bg(WHITE_TILE)
+    bext.clear()
+    
+    board = {'width': WIDTH, 'height': HEIGHT}
+    ants = []
+    for i in range(NUMBER_OF_ANTS):
+        ant = {
+            'x': random.randint(0, WIDTH-1),
+            'y': random.randint(0, HEIGHT-1),
+            'direction': random.choice([NORTH, SOUTH, EAST, WEST]),
+            }
+        ants.append(ant)
+        
+        changedTiles =[]
+        while True:
+            displayBoard(board, ants, changedTiles)
+            changedTiles = []
+            
+            nextBoard = copy.copy(board)
+            for ant in ants:
+                if board.get((ant['x'],ant['y']), False) == True:
+                    nextBoard [ (ant['x'],ant['y'])]   = False
+                    if ant['direction'] == NORTH:
+                        ant['direction'] = EAST
+                    elif ant['direction'] == EAST:
+                        ant['direction'] = SOUTH 
+                    elif ant['direction'] == SOUTH:
+                        ant['direction'] = WEST
+                    elif ant['direction'] == WEST:
+                        ant['direction'] = NORTH
+                else:
+                    nextBoard [ (ant['x'],ant['y'])]   = True
+                    if ant['direction'] == NORTH:
+                        ant['direction'] = WEST
+                    elif ant['direction'] == WEST:
+                        ant['direction'] = SOUTH 
+                    elif ant['direction'] == SOUTH:
+                        ant['direction'] = EAST
+                    elif ant['direction'] == EAST:
+                        ant['direction'] = NORTH
+                changedTiles.append((ant['x'],ant['y']))
+                if ant['direction'] == NORTH:
+                    ant['y'] -= 1
+                elif ant['direction'] == SOUTH:
+                    ant['y'] += 1
+                elif ant['direction'] == WEST:
+                    ant['x'] -= 1
+                elif ant['direction'] == EAST:
+                    ant['x'] += 1
+                ant['x'] = ant['x'] % WIDTH
+                ant['y'] = ant['y'] % HEIGHT
+                changedTiles.append((ant['x'],ant['y']))  
+            board = nextBoard
+            
+def displayBoard(board, ants, changedTiles):
+    for x, y in changedTiles:
+        bext.goto(x,y)
+        if board.get((x,y), False):
+            bext.bg(BLACK_TILE)
+        else:
+            bext.bg(WHITE_TILE)
+            
+        antIsHere = False
+        for ant in ants:
+            if (x,y) == (ant['x'],ant['y']):
+                antsIsHere = True
+                if ant['direction'] == NORTH:
+                    print(ANT_UP, end='')
+                elif ant['direction'] == SOUTH:
+                    print(ANT_DOWN, end='')
+                elif ant['direction'] == WEST:
+                    print(ANT_RIGHT, end='')
+                elif ant['direction'] == EAST:
+                    print(ANT_LEFT, end='')                
+        if not antIsHere:
+            print(' ', end='')       
+    bext.goto(0, HEIGHT)
+    bext.bg(WHITE_TILE)
+    print('Press Ctrl-C to quit', end='')
+    sys.stdout.flush()
+    time.sleep(PAUSE_AMOUNT)
+    
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Langton Ant')
+        sys.exit()
