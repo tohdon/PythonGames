056bfa8f226b5c7569c501acc3c27494c9f0a86a
tohdon <74751540+tohdon@users.noreply.github.com>
2022-06-06 06:31:50 +0800

    Add files via upload

---
+++ mondrian-art.py
@@ -0,0 +1,128 @@
+ 
+import random, sys
+import bext
+MIN_X_INCREASE= 6
+MAX_X_INCREASE = 16
+MIN_Y_INCREASE= 3
+MAX_Y_INCREASE = 6
+WHITE = 'white'
+BLACK = 'black'
+RED = 'red'
+YELLOW = 'yellow'
+BLUE = 'blue'
+width, height = bext.size()
+
+width -=1
+height -=3
+
+while True:
+    canvas = {}
+    for x in range(width):
+        for y in range(height):
+            canvas[(x,y)] = WHITE
+            
+    numberOfSegmentsToDelete = 0
+    x = random.randint(MIN_X_INCREASE,MAX_X_INCREASE)
+    while x< width - MIN_X_INCREASE:
+        numberOfSegmentsToDelete +=1
+        for y in range(height):
+            canvas[(x,y)] =BLACK
+        x += random.randint(MIN_X_INCREASE,MAX_X_INCREASE)
+    y = random.randint(MIN_Y_INCREASE,MAX_Y_INCREASE)
+    while y< height - MIN_Y_INCREASE:
+        numberOfSegmentsToDelete +=1
+        for x in range(width):
+            canvas[(x,y)] =BLACK
+        y += random.randint(MIN_Y_INCREASE,MAX_Y_INCREASE)
+    numberOfReactanglesToPaint = numberOfSegmentsToDelete - 3
+    numberOfSegmentsToDelete = int(numberOfSegmentsToDelete * 1.5)
+    print(str(len(canvas)))
+    for i in range(numberOfSegmentsToDelete):
+        while True:
+            startx = random.randint(1, width -2)
+            starty = random.randint(1,  height -2)
+            if canvas[(startx, starty)] == WHITE:
+                continue
+            if canvas[(startx - 1, starty)] == WHITE and canvas[(startx + 1, starty)] == WHITE:
+                orientation = 'vertical'
+            elif canvas[(startx, starty - 1 )] == WHITE and canvas[(startx, starty + 1 )] == WHITE:
+                orientation = 'horizontal'
+            else:
+                continue
+                
+            pointsToDelete = [(startx, starty)]
+                
+            canDeleteSegment = True
+            if orientation == 'vertical':
+                for changey in (-1, 1):
+                    y = starty
+                    while 0 < y < height -1:
+                        y += changey
+                        if canvas[(startx - 1, starty )]== BLACK and canvas[(startx + 1, starty )]== BLACK:
+                            break
+                        elif ((canvas[(startx -1 , starty )] == WHITE and canvas[(startx + 1, starty )]== BLACK) or
+                            (canvas[(startx -1 , starty )]== BLACK and canvas[(startx + 1, starty )] == WHITE)):
+                            canDeleteSegment = False
+                            break
+                        else:
+                            pointsToDelete.append((startx,y))
+                             
+            elif orientation == 'horizontal':
+                for changex in (-1, 1):
+                    x = startx
+                    while 0 < x < width -1 :
+                        x += changex
+                        if (canvas[(x, starty - 1)] == BLACK and canvas[(x, starty + 1)] == BLACK):
+                            break
+                        elif ((canvas[(x, starty - 1)] == WHITE and canvas[(x, starty + 1 )] == BLACK) or
+                                (canvas[(x, starty - 1)] == BLACK and canvas[(x, starty + 1)] == WHITE)):
+                            canDeleteSegment = False
+                            break
+                        else:
+                            pointsToDelete.append((x,starty))
+                            
+            if not canDeleteSegment:
+                continue
+            break
+                                      
+        for x, y in pointsToDelete:
+            canvas[(x, y)] = WHITE
+    for x in range(width):
+        canvas[(x,0)] = BLACK
+        canvas[(x,height - 1 )] = BLACK
+        
+    for y in range(height):
+        canvas[(0,y)] = BLACK
+        canvas[(width-1, y)] = BLACK
+                    
+    for i in range(numberOfReactanglesToPaint):
+        while True:
+            startx = random.randint(1, width -2)
+            starty = random.randint(1,  height -2)
+            if canvas[(startx, starty)] != WHITE:
+                continue
+            else:
+                break
+        colorToPaint = random.choice([RED,YELLOW, BLUE, BLACK])     
+        pointsToPaint = set([(startx, starty)])
+        while len(pointsToPaint) > 0:
+            x, y = pointsToPaint.pop()
+            canvas[(x,y)] = colorToPaint
+            if canvas[(x - 1, y)] == WHITE:
+                pointsToPaint.add((x - 1, y))
+            if canvas[(x + 1, y)] == WHITE:
+                pointsToPaint.add((x + 1, y))
+            if canvas[(x, y - 1)] == WHITE:
+                pointsToPaint.add((x, y - 1))
+            if canvas[(x, y + 1)] == WHITE:
+                pointsToPaint.add((x, y + 1))
+                
+    for y in range(height):
+        for x in range(width):
+            bext.bg(canvas[(x,y)])
+            print(' ', end='')
+        print()
+    try:
+        input('Please enter for another work of art or Ctrl-C to quit')
+    except KeyboardInterrupt:
+        sys.exit()
